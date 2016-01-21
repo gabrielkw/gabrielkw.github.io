28ff7ab78598863d24ac41b360e5f63f615a183b
muthdra <printfgabriel@gmail.com>
2016-01-21 17:45:56 -0200

    Alterações de layout

--- hash-escolar/js/main.js
+++ hash-escolar/js/main.js
@@ -1,16 +1,35 @@
-function calcular() {
-    hash_ordenada = true
-    i = document.getElementById("numero").value
-    document.getElementById("numero").value = ""
-    hash = i
+function raiz_quadrada_cinco_vezes(numero) {
     for (j = 0; j < 5; j++) {
-        hash = Math.sqrt(hash)
+         numero = Math.sqrt(numero)
     }
+    return numero
+}
+
+function seis_numeros_depois_da_virgula(numero){
+    hash = numero.toString()
+    hash = hash.replace(/0/g, "").substr(hash.lastIndexOf(".") + 1, 6)
+    return hash
+}
+
+function ordenar_hash(numero){
     hash = hash.toString()
-    hash = hash.replace(/0/g, "")
-    hash = hash.substr(hash.lastIndexOf(".") + 1, 6)
-    if (hash_ordenada) hash = hash.split("").sort().join("")
+    hash = numero.split("").sort().join("")
+    return hash
+}
+
+function calcular() {
+    numero_input = document.getElementById("numero")
+    numero = numero_input.value
+    numero_input.value = ""
+    
+    hash = numero
+    hash = raiz_quadrada_cinco_vezes(hash)
+    hash = seis_numeros_depois_da_virgula(hash)
+    hash = ordenar_hash(hash)
+    
     if (hash == "") hash = "0"
     resultado = document.getElementById("resultado")
-    resultado.innerHTML = "A hash do número " + i + " é: </br>" + hash
+    resultado.innerHTML = "A hash do número " + numero + " é: </br>" + hash
+    
+    numero_input.focus()
 }
